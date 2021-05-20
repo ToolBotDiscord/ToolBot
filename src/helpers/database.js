8f17fcecd42f821b41d0dda17606ca666dc8e5fb
EnderMC <thelio.trinite@outlook.com>
2021-05-20 18:15:42 +0200

    base de données

---
+++ src/helpers/database.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database(`${__dirname}/../toolbot.db`, sqlite3.OPEN_READWRITE, (err) => {
+  if (err) console.error(err.message);
+  console.log('Successfully connected to the database.');
+});
+
+// close the database connection
+// db.close((err) => {
+//   if (err) return console.error(err.message);
+//
+//   console.log('Successfully closed the database connection.');
+// });
+
+module.exports = db;
