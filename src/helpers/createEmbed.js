7622b4052e6769fee34647f9ab1be29ee5c3d02b
EnderMC <thelio.trinite@outlook.com>
2021-05-20 18:17:01 +0200

    embed helper

---
+++ src/helpers/createEmbed.js
@@ -0,0 +1,44 @@
+const { MessageEmbed } = require('discord.js');
+const { client } = require('../index');
+
+/**
+ * @param {{
+ *   author?: {
+ *     author: string,
+ *     iconURL?: string
+ *   },
+ *   title: string,
+ *   description?: string,
+ *   fields?: any,
+ *   footer?: {
+ *     text: string,
+ *     iconURL?: string
+ *   }
+ *   timestamp?: Date | number
+ * }} options
+ * @returns {module:"discord.js".MessageEmbed}
+ */
+const createEmbed = (options) => {
+  const author = options.author && {
+    author: options.author.author,
+    iconURL: options.author.iconURL
+  } || { name: null, iconURL: null };
+  const fields = options.fields && options.fields || [];
+  const footer = options.footer && {
+    text: options.footer.text,
+    iconURL: options.footer.iconURL
+  } || { name: null, iconURL: null };
+
+  if (typeof options.title === 'undefined') throw new Error('The embed title is not defined.');
+
+  return new MessageEmbed()
+    .setAuthor(author.author || '', author.iconURL || '')
+    .setTitle(options.title)
+    .setDescription(options.description && options.description || '')
+    .addFields(fields)
+    .setFooter(footer.text || client.user.username, footer.iconURL || client.user.avatarURL())
+    .setTimestamp(options.timestamp || Date.now())
+  ;
+};
+
+module.exports = createEmbed;
