a147c7149a33bd0245eae38dd1715b100423072f
EnderMC <thelio.trinite@outlook.com>
2021-05-24 17:47:38 +0200

    database helper

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql');
 
+const connectionConfig = {
+  host: process.env.NODE_ENV === 'production' ? '' : '127.0.0.1',
+  user: process.env.NODE_ENV === 'production' ? '' : 'root',
+  password: process.env.NODE_ENV === 'production' ? '' : 'root',
+  database: process.env.NODE_ENV === 'production' ? '' : 'toolbot'
+};
+
 /**
  * @type {Connection}
  */
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'toolbot'
-});
+const db = mysql.createConnection(connectionConfig);
 
 db.connect((err) => {
   if (err) throw err;
