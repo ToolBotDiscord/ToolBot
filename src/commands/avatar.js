074f9baea8ede04addd0e5d4da7562f7be6396d2
EnderMC <thelio.trinite@outlook.com>
2021-05-27 15:53:24 +0200

    commande avatar

---
+++ src/commands/avatar.js
@@ -0,0 +1,24 @@
+const helpCommand = require('../helpers/HelpCommandHelper');
+const InteractionDataHelper = require('../helpers/InteractionDataHelper');
+
+module.exports = {
+  help: helpCommand('avatar', 'Permet de récupérer l\'avatar d\'un utilisateur.', [
+    {
+      name: 'utilisateur',
+      description: 'L\'avatar d\'un utilisateur.',
+      type: 6,
+      required: false
+    }
+  ]),
+  run: (client, interaction, args) => {
+    let member;
+    args && args.utilisateur ? member = args.utilisateur : member = interaction.member;
+
+    const message = InteractionDataHelper(`Voici ${member === interaction.member ? 'votre avatar' : `l'avatar de ${interaction.member.user.username}`} :`);
+    // message.channel.send(msg, {
+    //   files: [{ attachment: member.user.displayAvatarURL(), name: 'avatar.png' }]
+    // });
+
+    return message;
+  }
+};
