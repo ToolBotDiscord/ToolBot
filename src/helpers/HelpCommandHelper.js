ba0dbfaadf9e72b99e238ab6971f2f88d73d4a56
EnderMC <thelio.trinite@outlook.com>
2021-05-27 19:41:14 +0200

    `run` -> `name`

--- src/helpers/HelpCommandHelper.js
+++ src/helpers/HelpCommandHelper.js
@@ -1,12 +1,12 @@
 /**
- * @param {string} run
+ * @param {string} name
  * @param {string} description
  * @param {[{}]} [options]
  * @param {[{ id: string, type: number, permission: boolean }]} [permissions]
- * @return {{ options: [{}], description: string, run: string, permissions: [{}] }}
+ * @return {{ options: [{}], description: string, name: string, permissions: [{}] }}
  */
-const HelpCommand = (run, description, options, permissions) => {
-  return { run, description, options, permissions };
+const HelpCommand = (name, description, options, permissions) => {
+  return { name, description, options, permissions };
 };
 
 module.exports = HelpCommand;
